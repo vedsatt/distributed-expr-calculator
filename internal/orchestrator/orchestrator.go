649de9432c7615540eceb6f3eeb92a1e6d1ef21d
vedsatt <retr0artem@mai.ru>
2025-05-04 18:11:45 +0300

    reworked checkCookie func

--- internal/orchestrator/orchestrator.go
+++ internal/orchestrator/orchestrator.go
@@ -54,11 +54,7 @@ func checkCookie(cookie *http.Cookie, err error) bool {
 	}
 
 	token := cookie.Value
-	if len(token) == 0 {
-		return false
-	}
-
-	return true
+	return len(token) == 0
 }
 
 func errorResponse(w http.ResponseWriter, err string, statusCode int) {
